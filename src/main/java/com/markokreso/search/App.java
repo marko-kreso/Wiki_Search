5726820387426433d09ba9004a47ce14439a3ce7
Marko Kreso <markokreso@gmail.com>
2021-10-28 21:34:36 -0700

    Project Start

---
+++ src/main/java/com/markokreso/search/App.java
@@ -0,0 +1,15 @@
+package com.markokreso.search;
+
+import org.apache.lucene.document.Document;
+
+/*
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
